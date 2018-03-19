01f41d4d8fe481aead9a2791cd65fa74a97c03ff
Sebastian Springer <info@sebastian-springer.com>
2018-03-19 16:34:48 +0100

    time component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimeComponent } from './time/time.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule
